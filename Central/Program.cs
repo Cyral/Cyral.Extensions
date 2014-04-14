98e3b5f9815310d0be6a5167ccd498487843e696
Cyral <cyral@zarknorth.com>
2014-04-14 19:54:49 -0400

    Central/Testing project

---
+++ Central/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cyral.Extensions;
+using Cyral.Extensions.Xna;
+
+namespace Central
+{
+    /// <summary>
+    /// Test code from the extension library in this project.
+    /// Do not worry about file changes, all changes in this file
+    /// will be ignored in git.
+    /// </summary>
+    class Program
+    {
+        //Possibly useful
+        private static Random random = new Random();
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Cyral.Extensions Testing Area");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //Do Stuff
+
+            Console.ReadLine();
+        }
+    }
+}
